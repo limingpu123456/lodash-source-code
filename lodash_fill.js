060cf6b2544584e2591767b7e7debcd0ec241a56
李小浦 <85180586@qq.com>
2023-10-31 08:36:49 +0800

    Update lodash_fill.js
    Note -> 注意

--- lodash_fill.js
+++ lodash_fill.js
@@ -1,5 +1,5 @@
 //fill使用 value 值来填充（替换） array，从start位置开始, 到end位置结束（但不包含end位置）。
-//Note: 这个方法会改变 array（注：不是创建新数组）。
+//注意: 这个方法会改变 array（注：不是创建新数组）。
 
 function fill(array,value,start = 0, end = array.length){
     for(let i = start ; i < end ; i++){
